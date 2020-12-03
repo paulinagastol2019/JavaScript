768b9ad2df52d5e6a294fdce429f6e13ecdeaf43
Paulina <paulina09krk@gmail.com>
2020-12-03 19:17:45 +0100

    next exercise

--- 2. exercise 2 - menu hamburger/js.js
+++ 2. exercise 2 - menu hamburger/js.js
@@ -1,6 +1,11 @@
 const burger = document.querySelector(".burger");
+const iconBurger = document.querySelector(".fa-bars");
+const iconX = document.querySelector(".fa-times");
+const column = document.querySelector("aside");
+
 burger.addEventListener("click", function(){
 
-burger.classList.toggle("active") // zabiera i dodaje klasę
-
+iconBurger.classList.toggle("show"); // wyłączenie klasy
+iconX.classList.toggle("show"); // dodanie klasy
+column.classList.toggle("show"); // dodanie klasy
 })
