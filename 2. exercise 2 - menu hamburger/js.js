516119fda304ab4cea6be4d1ba71caad251bf057
Paulina <paulina09krk@gmail.com>
2020-12-03 19:43:32 +0100

    change to jQuery

--- 2. exercise 2 - menu hamburger/js.js
+++ 2. exercise 2 - menu hamburger/js.js
@@ -1,11 +1,16 @@
-const burger = document.querySelector(".burger");
-const iconBurger = document.querySelector(".fa-bars");
-const iconX = document.querySelector(".fa-times");
-const column = document.querySelector("aside");
+// const burger = document.querySelector(".burger");
+// const iconBurger = document.querySelector(".fa-bars");
+// const iconX = document.querySelector(".fa-times");
+// const column = document.querySelector("aside");
 
-burger.addEventListener("click", function(){
+// burger.addEventListener("click", function(){
 
-iconBurger.classList.toggle("show"); // wyłączenie klasy
-iconX.classList.toggle("show"); // dodanie klasy
-column.classList.toggle("show"); // dodanie klasy
+// iconBurger.classList.toggle("show"); // wyłączenie klasy
+// iconX.classList.toggle("show"); // dodanie klasy
+// column.classList.toggle("show"); // dodanie klasy
+// })
+
+$(".burger").on("click", function () {
+    $(".fas, aside, .wrapper").toggleClass("show");
+
 })
